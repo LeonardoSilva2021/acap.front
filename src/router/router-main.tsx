77d4d8161be8ced5967eadff0d8d706096f19265
Leonardo da Silva Gonçalves <86031948+LeonardoSilva2021@users.noreply.github.com>
2024-11-25 21:48:57 -0300

    Correção do router

--- src/router/router-main.tsx
+++ src/router/router-main.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createHashRouter, RouterProvider } from "react-router-dom"
 import { MainPage } from "../view/page/private/main/main-page"
 import { HomePage } from "../view/page/private/home/home-page"
 import { DownloadsPage } from "../view/page/private/downloads/downloads-page"
@@ -6,7 +6,7 @@ import { SobreNosPage } from "../view/page/private/sobre-nos/sobre-nos-page"
 
 export const RouterMain = () => {
 
-    const router = createBrowserRouter([
+    const router = createHashRouter([
         {
             
             path: '/',
@@ -40,9 +40,7 @@ export const RouterMain = () => {
                 </MainPage>
             )
         }
-    ], {
-        basename: '/acap.front'
-    });
+    ]);
 
     return (
         <>
